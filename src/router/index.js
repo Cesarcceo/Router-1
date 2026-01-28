20f24a7c43c32e930575eb83535d0db223f41c8e
Cesacceo <cesar.consuelo@cceo.com.mx>
2026-01-28 16:15:16 -0600

    New Route: :sparkles: a new route added

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/Pokemons',
+      name: 'Pokemons',
+      component: () => import('../views/PokemonsView.vue')
+    },
   ],
 })
 
