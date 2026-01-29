2d30cb6f9d1244af1585f469f1c004360fd096a8
Cesacceo <cesar.consuelo@cceo.com.mx>
2026-01-29 09:34:01 -0600

    New comosable (composables): :sparkles: getData.js was created

---
+++ src/composables/getData.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { ref } from "vue";
+
+export const useGetData = () => {
+
+    const data = ref(null);
+    const loading = ref(true);
+
+    const getData = async (url) => {
+        loading.value = true;
+        try {
+            const response = await axios.get(url);
+            data.value = response.data;
+        } catch (error) {
+            console.log(error);
+        }finally{
+            setTimeout(() => loading.value = false, 2000)
+        }
+    }
+    return {
+        data,
+        loading,
+        getData,
+    };
+};
