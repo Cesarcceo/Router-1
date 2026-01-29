ce5d6e14a13b88f1d6fca7a39cc4b68c85dafbfc
Cesacceo <cesar.consuelo@cceo.com.mx>
2026-01-29 11:49:48 -0600

    New feature (FavoritosView.vue): :sparkles: button Mas informacion added

--- src/views/FavoritosView.vue
+++ src/views/FavoritosView.vue
@@ -1,11 +1,17 @@
 <script setup>
+import { useRouter } from 'vue-router';
 import { useFavoritoStore } from '@/store/favoritos';
 import { storeToRefs } from 'pinia';
 
+const router = useRouter()
 const useFavorito = useFavoritoStore()
 
 const {remove} = useFavorito
 const {favoritos} = storeToRefs(useFavorito)
+
+const back = name => {
+    router.push(`/pokemons/${name}`)
+}
 </script>
 
 <template>
@@ -16,7 +22,8 @@ const {favoritos} = storeToRefs(useFavorito)
                 {{ poke.name }}
             </div>
             <div>
-                <button class="btn btn-sm btn-danger" @click="remove(poke.id)">Eliminar</button>
+                <button class="btn btn-sm btn-danger me-2" @click="remove(poke.id)">Eliminar</button>
+                <button class="btn btn-sm btn-primary" @click="back(poke.name)" >Mas informacion</button>
             </div>
         </li>
     </ul>
