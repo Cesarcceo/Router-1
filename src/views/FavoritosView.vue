ea724c2444a71ddecc9126b6c0748390ec988fcd
Cesacceo <cesar.consuelo@cceo.com.mx>
2026-01-29 11:26:57 -0600

    New featue (FavoritosView.vue): :sparkles: now the pokemons favoritos can be removed

--- src/views/FavoritosView.vue
+++ src/views/FavoritosView.vue
@@ -4,6 +4,7 @@ import { storeToRefs } from 'pinia';
 
 const useFavorito = useFavoritoStore()
 
+const {remove} = useFavorito
 const {favoritos} = storeToRefs(useFavorito)
 </script>
 
@@ -11,7 +12,12 @@ const {favoritos} = storeToRefs(useFavorito)
     <h1>Pokemons Favoritos</h1>
     <ul class="list-group">
         <li class="list-group-item" v-for="poke in favoritos" :key="poke.id">
-            {{ poke.name }}
+            <div>
+                {{ poke.name }}
+            </div>
+            <div>
+                <button class="btn btn-sm btn-danger" @click="remove(poke.id)">Eliminar</button>
+            </div>
         </li>
     </ul>
 </template>
