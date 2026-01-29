fd859f1d623c006d2300598d08703bc3f91f7578
Cesacceo <cesar.consuelo@cceo.com.mx>
2026-01-29 10:51:41 -0600

    New feature (HomeView.vue): :sparkles: used destructuring

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,17 @@
 <script setup>
+import { storeToRefs } from 'pinia'
 import TheWelcome from '../components/TheWelcome.vue'
 import { usercounterStore } from '@/store/counter'
 const useCounter = usercounterStore()
+
+const {increment} = useCounter
+const {count, double} = storeToRefs(useCounter)
 </script>
 
 <template>
-  <h1>Home Counter: {{ useCounter.count }}</h1>
-  <h2>Double: {{ useCounter.double }}</h2>
-  <button @click="useCounter.increment">increment</button>
+  <h1>Home Counter: {{ count }}</h1>
+  <h2>Double: {{ double }}</h2>
+  <button @click="increment">increment</button>
   <main>
     <TheWelcome />
   </main>
