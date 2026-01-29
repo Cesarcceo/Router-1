dcc6d961d955e3739f2bb720e8fe223f810536a5
Cesacceo <cesar.consuelo@cceo.com.mx>
2026-01-29 11:25:20 -0600

    New feature (favoritos.js): :sparkles: two new functions

--- src/store/favoritos.js
+++ src/store/favoritos.js
@@ -7,8 +7,17 @@ export const useFavoritoStore = defineStore('favoritos', ()=> {
     const add = poke => {
         favoritos.value.push(poke)
     }
+
+    const remove = (id) =>{
+        favoritos.value = favoritos.value.filter(item => item.id !== id)
+    }
+
+    const findPoke = name => favoritos.value.find(item => item.name === name)
+
     return {
         favoritos,
-        add
+        add,
+        remove,
+        findPoke,
     }
 })
