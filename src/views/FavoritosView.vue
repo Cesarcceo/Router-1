84e62e8f11087309ee102acd367d6c29af4a90c7
Cesacceo <cesar.consuelo@cceo.com.mx>
2026-01-29 11:13:19 -0600

    New feature (FavoritosView.vue): :sparkles: the pokemons favoritos now show

--- src/views/FavoritosView.vue
+++ src/views/FavoritosView.vue
@@ -1,7 +1,17 @@
 <script setup>
+import { useFavoritoStore } from '@/store/favoritos';
+import { storeToRefs } from 'pinia';
 
+const useFavorito = useFavoritoStore()
+
+const {favoritos} = storeToRefs(useFavorito)
 </script>
 
 <template>
-    <h1>Favoritos</h1>
+    <h1>Pokemons Favoritos</h1>
+    <ul class="list-group">
+        <li class="list-group-item" v-for="poke in favoritos" :key="poke.id">
+            {{ poke.name }}
+        </li>
+    </ul>
 </template>
