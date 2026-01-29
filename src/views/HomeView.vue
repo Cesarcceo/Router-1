6cb6414ad1d0f54968fab2629ae5ac7c96ce1ad9
Cesacceo <cesar.consuelo@cceo.com.mx>
2026-01-29 10:42:22 -0600

    New feature (HomeView.vue): :saprkles: counter store was implemented

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,13 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import { usercounterStore } from '@/store/counter'
+const useCounter = usercounterStore()
 </script>
 
 <template>
+  <h1>Home Counter: {{ useCounter.count }}</h1>
+  <h2>Double: {{ useCounter.double }}</h2>
+  <button @click="useCounter.increment">increment</button>
   <main>
     <TheWelcome />
   </main>
